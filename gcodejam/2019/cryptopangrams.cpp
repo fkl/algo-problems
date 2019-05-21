3dcbc24bd2b5ce2ba1103c405f1a649711ba87eb
fkl <fayyazkl@gmail.com>
2019-05-20 18:13:18 -0700

    Fixed the missing prime 2 mistake
    1. Debug code to log when you failed finding factors for a given prime
    2. Added 2 to the sieve prime computation list
    3. The above effectively fixed the one letter behind case (i.e. char A seemed correct but any latter ones like S was appearing as R because we were missing 2 from the prime array.

--- gcodejam/2019/cryptopangrams.cpp
+++ gcodejam/2019/cryptopangrams.cpp
@@ -14,10 +14,15 @@
 #include <stdio.h>      /* printf */
 #include <stdlib.h>     /* qsort */
 #include <set>
-
+#include <iomanip> // for setw
 using namespace std;
 
-//#define SIZE 100
+// Stuff kept global temporarily (really bad idea but was just fast during contest)
+// The starting and ending indexes of primes used in current test case
+int start_idx, end_idx;
+set<long> s; // to avoid duplicated primes as well as maintain them in oder
+vector<long> primes_orig; // primes in original order in which they appear in the encrypted string
+vector<long> sorted_primes; // set converted to array for convenience
 
 // upper is the largest number up to which we find primes. The number of primes smaller than upper should be less than size of array primes[]
 int EratosthenesSieve (int upper, int primes[])
@@ -30,6 +35,10 @@ int EratosthenesSieve (int upper, int primes[])
 	memset(sieve,0,sizeof sieve);
 	int tmp;
 	// ignore 2 as prime i.e. instead of starting i from 2, start from 3 and instead of i++, do i+=2
+	
+	// UPDATE: BIG MISTAKE was ignoring two, found large values which could only be factored using 2
+	primes[0] = 2;
+	size++;
 	for(int i(3); i<=upper; i+=2)
 	{
 		if(!sieve[i])
@@ -46,26 +55,37 @@ int EratosthenesSieve (int upper, int primes[])
 	return size;
 }
 
-// The starting and ending indexes of primes used in current test case
-int start_idx, end_idx;
-set<long> s;
+int index_in_prime_set(long prime) {
+	
+	int idx=0;
+	
+	for(auto it=s.begin(); it!=s.end(); ++it, ++idx) {
+		if(*it == prime) {
+			cout << " Index is: " << idx;
+			return idx;
+		}
+	}
+	
+	return -1;
+}
 
 // Following function finds two prime factors that are multipled to get the corresponding original text letter
 // 217 shoudl result in CJ (C=7 and J=31)
 // last p is the common multiplier that has to be used next i.e. J in the above example to be used next
-vector<char> compute_actual_facts(long cipher, vector<int> primes_used) {
+vector<char> compute_actual_facts(long cipher) {
 	
 	// Now we only iterate through the 26 primes actually used from the set
 	
-	int left = 0;
-	int right = (int)primes_used.size();
+	long left = 0;
+	long right = sorted_primes.size()-1;
+	
 	vector<char> r;
 	
 	//cout << "right is: " << right << endl;
 	while(left < right) {
 		
-		long pl = primes_used[left];
-		long pr = primes_used[right];
+		long pl = sorted_primes[left];
+		long pr = sorted_primes[right];
 		
 		if(pl*pr < cipher) {
 			left++;
@@ -77,14 +97,14 @@ vector<char> compute_actual_facts(long cipher, vector<int> primes_used) {
 		
 		else { // when equal
 			char f, s;
-			f = 'A' + left;
-			s = 'A' + right;
+			f = 'A' + index_in_prime_set(pl); //+ 1;
+			s = 'A' + index_in_prime_set(pr); //+ 1;
 			
 			if(isupper(f) && isupper(s)){ // only if both are valid capital charters
 				r.push_back(f);
 				r.push_back(s);
 				
-				cout << "First letter: " << f << "\t and Second letter: " << s << endl;
+				cout << " First: " << f << "\t Second: " << s << endl;
 				break;
 			}
 		}
@@ -107,27 +127,14 @@ int map_p_to_a(long p, int prime[], int max_prime) {
 			break;
 	}
 	
-	// Below is earlier original order of end to start
-	/*for(c=max_prime-1; c>=0; --c, cnt++) {
-		if(prime[c] == p) {
-			break;
-		}
-	}*/
-	
-	//cout << "Index:" << c << "\tPrime is:" << prime[c] << endl;
 	return c;
 }
 
-// Following function finds two prime factors that are multipled to get the corresponding original text letter
-// 217 shoudl result in CJ (C=7 and J=31)
-// last p is the common multiplier that has to be used next i.e. J in the above example to be used next
-
-// Update: Because we don't know which primes are used, this function now only finds which 26 primes
+// Because we don't know which primes are used, this function only finds which 26 primes
 // to use and updates start and end indexes from the primes array. Actually finding the facts is
 // moved to another function compute_actual_facts()
-vector<char> find_primes_used(long cipher, int primes[], int max_prime) {
+ void find_primes_used(long cipher, int primes[], int max_prime) {
 	
-	vector<char> r;
 	// pick two extreme end primes first and last
 	// if their product is equal to cipher return them, if less increment first, if greater decrement last
 	
@@ -135,7 +142,7 @@ vector<char> find_primes_used(long cipher, int primes[], int max_prime) {
 	int right = max_prime-1;
 	int left = 0;
 	
-	while(left < right) {
+	while(left <= right) {
 		
 		long pl =primes[left];
 		long pr = primes[right];
@@ -144,49 +151,46 @@ vector<char> find_primes_used(long cipher, int primes[], int max_prime) {
 		else if(pl*pr > cipher)
 			right--;
 		else { // product of two primes is equal here
-			
-			cout << "Encrypted text: " << cipher << "   Primes used are: " << pl << " and " << pr << endl;
-			
+
 			int f_id = map_p_to_a(pl, primes, max_prime);
 			int s_id = map_p_to_a(pr, primes, max_prime);
-			
+
+			cout << "Enc val" << setw(8) << cipher << " Primes are: " << setw(4) << pl << "(idx:"<< setw(4) << f_id
+			<< ") and " << setw(4) << pr << "(idx:" << setw(4) << s_id << ")" <<  endl;
+
 			if(f_id < start_idx)
 				start_idx = f_id;
 			
 			if(s_id > end_idx)
 				end_idx = s_id;
 			
+/*			long last_pm = primes_orig.size()-1;
+			
+			if(last_pm > 0) {
+				if(primes_orig[last_pm] != pl && primes_orig[last_pm-1] != pl)*/
+					primes_orig.push_back(pl);
+			//	if(primes_orig[last_pm] != pr && primes_orig[last_pm-1] != pr)
+					primes_orig.push_back(pr);
+			//}
 			s.insert(pl);
 			s.insert(pr);
 			
 			break;
 		}
 	}
-
-	return r;
+	 
+	if(left>right)
+		cout << "Failed to find prime factors for cipher: " << cipher << endl;
 }
 
-// problem from google code jam 2019
 void Cryptopangrams() {
-	// Test code for finding and printing first s primes
-	/*int arr[SIZE];
-	 int largest_prime = 100;
-	 int s = EratosthenesSieve(largest_prime, arr);
-	 cout << "Numer of primes s less than " << largest_prime <<  " is: " << s << endl;
-	 
-	 for(int c=0; c<s; ++c) {
-	 cout << arr[c] << "\t";
-	 }
-	 cout << endl;*/
-	
 
 	 int num_tests = 1;
 	 //cin >> num_tests;
-	 
-	 
+
 	for(int idx=1; idx<=num_tests; ++idx) {
 		int n, l;
-		vector<int>cipher_text;
+		//vector<int>cipher_text;
 		
 		//cin >> n >> l;
 		//n = 103;
@@ -195,89 +199,51 @@ void Cryptopangrams() {
 		l = 25;
 		// find at least 26 primes less than n
 		int primes[10000];
-		//int cnt_primes =
 		int prime_cnt = EratosthenesSieve(n, primes);
-		//cout << "Num primes: " << prime_cnt << endl;
 		
 		start_idx = 1000000;
 		end_idx = -1;
 		
 		// 103 25
 		/*int val_arr[] = {217, 1891, 4819, 2291, 2987, 3811, 1739, 2491, 4717, 445, 65, 1079, 8383, 5353, 901, 187, 649, 1003, 697, 3239, 7663, 291, 123, 779, 1007, 3551, 1943, 2117, 1679, 989, 3053};*/
-		
-		
+
 		//10000 25
 		int val_arr[] = {3292937, 175597, 18779, 50429, 375469, 1651121, 2102, 3722, 2376497, 611683, 489059, 2328901, 3150061, 829981, 421301, 76409, 38477, 291931, 730241, 959821, 1664197, 3057407, 4267589, 4729181, 5335543};
-		
 		
 		string result = "";
 		vector<char> r;
 		int list_i;
-		//char last_prime = ' ';
 		for(list_i=0; list_i<l; ++list_i) {
+			
 			int val;
 			//cin >> val;
 			val = val_arr[list_i];
-			cipher_text.push_back(val);
 			
-			r = find_primes_used(val, primes, prime_cnt);
-			
-			//if(r.size() < 2){
-			//	cout << "Failed to find 2 primes with the product" << endl;
-			//exit(1);
-			//}
-			
-			// Some times the common prime between the two is not the first one.
-			// E.g. QUIZ (Find facts would return IU in order, but the common prime multiplier is I, not U)
-			// Following if else fixes that.
-			/*if(last_prime != ' ') {
-			 
-			 if(r[0] != last_prime) {
-			 result += r[1];
-			 last_prime = r[0];
-			 }
-			 
-			 else if(r[0] == last_prime) {
-			 result += r[0];
-			 last_prime = r[1];
-			 }
-			 }
-			 
-			 else {
-			 result += r[0];
-			 last_prime = r[1];
-			 }*/
+			find_primes_used(val, primes, prime_cnt);
 		}
-			
-		//result += r[1];
-
-		vector<int> primes_used(s.begin(), s.end());
 		
+		sorted_primes.assign(s.begin(), s.end());
+
 		// loop again from start to end of the cipher text and decode, now that we know which primes to use
-		/*for(list_i=0; list_i<l; ++list_i) {
-			compute_actual_facts(val_arr[list_i], primes_used);
-			
-		}*/
+		for(list_i=0; list_i<l; ++list_i) {
+			compute_actual_facts(val_arr[list_i]);
+		}
 
 		
-		cout << "Start idx:" << start_idx << "\tEnd idx:" << end_idx << endl;
+		/*cout << "Start idx:" << start_idx << "\tEnd idx:" << end_idx << endl;
 		
 		int i_p=0;
-		//int p=1531; // looks a random test limit, not needed really
-		for(auto it = 0; it < primes_used.size(); it++,i_p++){
+		for(auto it = 0; it < primes_orig.size(); it++,i_p++){
 			
-			cout << primes_used[it] << " ";
+			cout << primes_orig[it] << " ";
 		}
-		//cout << "\ncount of indexes: " << i_p << endl;
-		//cout << primes_used.size() << endl;
-		//cout << s.count(89) << endl;
 		
-		/*cout << "Comparing with primes from original array" << endl;
+		cout << "\nComparing with primes from set" << endl;
 		
-		for(int idx=start_idx; idx<=end_idx; ++idx) {
-			cout << primes[idx] << " ";
-		}
-		*/
+		for(auto it=sorted_primes.begin(); it!=sorted_primes.end(); ++it) {
+			cout << *it << " ";
+		}*/
+		
 		cout << endl;
 		
 		//cout << "Case #" << num_tests << ": " << endl; // << result << endl;
@@ -290,6 +256,5 @@ void Cryptopangrams() {
 int main() {
 	
 	Cryptopangrams();
-	
 	return 0;
 }
